3d3e1d2452e31bab2d2293b3d1a07a46891470ee
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-12-16 15:28:08 +0700

    upload bài 3

---
+++ bài 3/Untitled2.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+void jung(int put1, int put2);
+void jung2(int *put1,int *put2);
+int main(int art, const char * arg[]){
+     int num1=20;
+     int num2=30;
+     int total = num1+num2; 
+     int temt; 
+     printf("tong la %d", total); 
+     return 0;
+}
